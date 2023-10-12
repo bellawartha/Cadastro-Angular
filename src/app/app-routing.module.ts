3febae0660a192d2718f0961a28187e068030687
Isabella Wartha <isabella.wartha@gmail.com>
2023-10-12 18:54:47 -0300

    project template

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CustomersListComponent } from './pages/customers-list/customers-list.component';
+import { CustomersEditComponent } from './pages/customers-edit/customers-edit.component';
+
+const routes: Routes = [  { path: '', component: CustomersListComponent},
+{ path: 'customersList', component: CustomersListComponent },
+{ path: 'customersEdit', component: CustomersEditComponent }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CustomersEditComponent } from './pages/customers-edit/customers-edit.component';
+import { CustomersListComponent } from './pages/customers-list/customers-list.component';
+import { FormComponent } from './pages/customers-edit/form/form.component';
+import { ListComponent } from './pages/customers-list/list/list.component';
+import { AddClientComponent } from './pages/customers-list/add-client/add-client.component';
+import { HeaderComponent } from './sharedComponents/header/header.component';
+import { FooterComponent } from './sharedComponents/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustomersEditComponent,
+    CustomersListComponent,
+    FormComponent,
+    ListComponent,
+    AddClientComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
